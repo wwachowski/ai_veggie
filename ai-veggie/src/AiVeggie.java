de9e409f2fa377e85d3bf78572755e89e63e634e
kubaluzak <jakub>
2024-12-19 22:52:23 +0100

    xd commit3

--- ai-veggie/src/AiVeggie.java
+++ ai-veggie/src/AiVeggie.java
@@ -26,7 +26,7 @@ class AiVeggie implements ActionListener
     {
         try
         {
-        	resources = ResourceBundle.getBundle("resources.Resources", Locale.forLanguageTag("en"));
+        	resources = ResourceBundle.getBundle("resources.Resources");
         }
         catch (MissingResourceException mre)
         {
@@ -62,7 +62,7 @@ class AiVeggie implements ActionListener
 
         clips = new Environment();
 
-        clips.load("ai-veggie\\clips\\what_should_i_eat.clp");
+        clips.load("C:\\Users\\jakub\\Desktop\\ai_veggie\\ai-veggie\\clips\\what_should_i_eat.clp");
 
         clips.reset();
 
